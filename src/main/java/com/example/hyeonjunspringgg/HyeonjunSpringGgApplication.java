916e7a4a4860918ceab57012f18a5c90e5c5da22
Hyeonjun Park <phjppo0918@gmail.com>
2022-09-23 16:49:28 +0900

    feat: init setting

---
+++ src/main/java/com/example/hyeonjunspringgg/HyeonjunSpringGgApplication.java
@@ -0,0 +1,13 @@
+package com.example.hyeonjunspringgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyeonjunSpringGgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyeonjunSpringGgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hyeonjunspringgg/HyeonjunSpringGgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hyeonjunspringgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyeonjunSpringGgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
